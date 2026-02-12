84565cbd6f805465447cf89da18253ff9d0a62f1
contatoyvesx <contato.yvesx@gmail.com>
2026-02-12 19:48:38 -0300

    feat: improve signal message template for higher CTR

--- test/signal-message.test.js
+++ test/signal-message.test.js
@@ -1,14 +1,10 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 
-import { LINK, montarMensagemSinal, validarMensagemSinal } from "../bot.js";
+import { LINK, build_signal_message, validarMensagemSinal } from "../bot.js";
 
 test("mensagem do sinal contém termos obrigatórios e link afiliado", () => {
-  const mensagem = montarMensagemSinal({
-    entrada: "14:32",
-    saque: "2.15",
-    link: LINK,
-  });
+  const mensagem = build_signal_message("14:32", "2.15", LINK);
 
   assert.equal(validarMensagemSinal(mensagem, LINK), true);
   assert.match(mensagem, /AVIATOR/);
